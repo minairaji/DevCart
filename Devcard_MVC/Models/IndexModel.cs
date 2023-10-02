1941033fc852328903d8bc2798306364a699e5b1
user <user@DESKTOP-2FBBS9T>
2023-10-02 16:20:50 +0330

    create partial view

---
+++ Devcard_MVC/Models/IndexModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Devcard_MVC.Models
+{
+    public class IndexModel
+    {
+        /// <summary>
+        /// model madar ke jofte indo ra ersal konim
+        /// ke list az ha 2kelas
+        /// </summary>
+     public List<Project> projects { get; set; }
+     public List<Article> Article { get; set; }
+    }
+}
