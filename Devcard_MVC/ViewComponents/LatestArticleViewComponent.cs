0c7cf849448fbad6e07ac5fd3fbcdee54a7c2171
user <user@DESKTOP-2FBBS9T>
2023-10-03 00:18:28 +0330

    project last article

---
+++ Devcard_MVC/ViewComponents/LatestArticleViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Devcard_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Devcard_MVC.ViewComponents
+{
+    public class LatestArticleViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var articles = new List<Article>
+            {
+new Article(1,"هوش مصنوعی","محصولات ایجاد شده با ","blog-post-thumb-1.jpg"),
+               new Article (2,"هوش مصنوعی","محصولات ایجاد شده با ","blog-post-thumb-3.jpg"),
+               new Article (3,"هوش مصنوعی","محصولات ایجاد شده با ","blog-post-thumb-5.jpg"),
+               new Article (4,"هوش مصنوعی","محصولات ایجاد شده با ","blog-post-thumb-7.jpg"),
+            };
+            return View("_LatestArticle",articles);
+            //return View("",articles);
+        }
+    }
+}
---
+++ Devcard_MVC/ViewComponents/ProjectsViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using Devcard_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Devcard_MVC.ViewComponents
+{
+    public class ProjectsViewComponent : ViewComponent
+    {
+       public IViewComponentResult Invoke()
+        {
+            var projects = new List<Project>
+            {
+                ///read from viewcomponent
+                new Project(1,"تاکسی","پروژه انلاین تاکسی","project-1.jpg","iraji"),
+                new Project(2,"ارایشگاه","پروژه انلاین ارایشگاه","project-4.jpg","پریا"),
+                new Project(3,"ازمون ","پروژه انلاین ازمون فنی حرفه ای","project-figure-1.jpg","سازمان"),
+                new Project(4,"فروشگاه","پروژه انلاین فروشگاه","project-5.jpg","iraji")
+            };
+            return View("_Projects",projects);
+        }
+        /*public IViewComponentResult Invoke()
+        {
+            return View("_Projects");
+        }*/
+    }
+}
