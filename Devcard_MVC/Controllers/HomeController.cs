57655bb6b0a9f35e9f433d1fd27f51fdd108942a
user <user@DESKTOP-2FBBS9T>
2023-10-03 20:33:14 +0330

    link nav to contact

--- Devcard_MVC/Controllers/HomeController.cs
+++ Devcard_MVC/Controllers/HomeController.cs
@@ -17,6 +17,10 @@ namespace Devcard_MVC.Controllers
         {
             return View();
         }
+        public IActionResult Contact()
+        {
+            return View();
+        }
 
         
 
---
+++ Devcard_MVC/Controllers/TestController1.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Threading.Tasks;
+
+namespace Devcard_MVC.Controllers
+{
+    public class TestController1 : Controller
+    {
+        //public FileResult Index()
+        //{
+        //    ///return File("~/test.txt", "text/html");
+        //    var fileByte = System.IO.File.ReadAllBytes("wwwroot/test.txt");
+        //    const string fileName = "salamtest.txt";
+        //    return File(fileByte, MediaTypeNames.Text.Plain, fileName);
+        //}
+        //public JsonResult Index()
+        //{
+        //    return Json( new
+        //    {
+        //        id=1,
+        //        name="ali",
+        //        job="programmer",
+        //        site="yamour.com"
+        //    });
+        //}
+        //public RedirectResult Index()
+        //{
+        //    return Redirect("http:\\www.sanjesh.org");
+        //}
+        public IActionResult Index()
+        {
+            return new OkResult();
+            //return new BadRequestResult();
+            //return new NotFoundResult();
+        }
+    }
+}
